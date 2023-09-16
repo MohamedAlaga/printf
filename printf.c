fc0021467b5bb8f99deae6c27e884746cce281d0
MohamedAlaga <malaga.142014@gmail.com>
2023-09-16 23:53:36 +0300

    pls one more

--- printf.c
+++ printf.c
@@ -1,39 +1,35 @@
 #include"main.h"
 
-int _printf(const char *format, ...){
-	int counter = 0;
-	int templen = 0;
-	va_list args;
-	char *temp;
-    int tempchar;
+int _printf(const char *format, ...) {
+  int counter = 0;
+  int tempchar;
+  va_list args;
 
-	
-	va_start(args,format);
-	while (*format)
-	{
-		if (*format != '%')
-        {
-	       	write(1,format,1);
-		counter ++;
-		format++;
-        }else
-        {
-         format++;
-         if (*format == 'c')
-         {
-             format++;
-             tempchar = va_arg(args,int);
-             write(1, &tempchar,4);
-	     counter++;
-         } else if (*format == 's')
-         {
-             format++;
-             temp = va_arg(args,char*);
-             templen = strlen(temp);
-             write(1, temp,templen);
-	     counter++;
-         }
-         }
-        }
-	return counter;
+  va_start(args, format);
+  while (*format) {
+    if (*format != '%') {
+      write(1, format, 1);
+      counter++;
+      format++;
+    } else {
+      format++;
+      if (*format == 'c') {
+        format++;
+        tempchar = va_arg(args, int);
+        write(1, &tempchar, 4);
+        counter++;
+      } else if (*format == 's') {
+        format++;
+        char *temp = va_arg(args, char *);
+        int templen = strlen(temp);
+        write(1, temp, templen);
+        counter++;
+      } else {
+        write(1, format, 1);
+        format++;
+        counter++;
+      }
+    }
+  }
+  return counter;
 }
