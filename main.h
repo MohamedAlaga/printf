ae9aed5d506602477372acc2017df8caef113fea
MohamedAlaga <malaga.142014@gmail.com>
2023-09-16 22:50:53 +0300

    done

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdarg.h>
-
+int _printf(const char *format, ...);
 
 
 #endif
--- printf.c
+++ printf.c
@@ -1,24 +1,24 @@
 
-void print(char *str, ...){
+int _printf(const char *format, ...){
     va_list args;
-    va_start(args,str);
-    while (*str)
+    va_start(args,format);
+    while (*format)
     {
-        if (*str != '%')
+        if (*format != '%')
         {
-            write(1,str,1);
-            str++;
+            write(1,format,1);
+            format++;
         }else
         {
-         str++;
-         if (*str == 'c')
+         format++;
+         if (*format == 'c')
          {
-             str++;
+             format++;
              char* temp = va_arg(args,char*);
              write(1, temp,1);
-         } else if (*str == 's')
+         } else if (*format == 's')
          {
-             str++;
+             format++;
              char* temp = va_arg(args,char*);
              int templen = strlen(temp);
              write(1, temp,templen);
