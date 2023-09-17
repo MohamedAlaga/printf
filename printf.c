1554609f296ed6d62044646a489d93b098f587c6
mashaheen <finalflash147@yahoo.com>
2023-09-17 05:26:25 -0700

    betty/otherFixes

--- printf.c
+++ printf.c
@@ -5,35 +5,39 @@ int _printf(const char *format, ...){
 	int templen = 0;
 	va_list args;
 	char *temp;
-    int tempchar;
+	int tempchar;
 
-	
 	va_start(args,format);
 	while (*format)
 	{
 		if (*format != '%')
-        {
-	       	write(1,format,1);
-		counter ++;
-		format++;
-        }else
-        {
-         format++;
-         if (*format == 'c')
-         {
-             format++;
-             tempchar = va_arg(args,int);
-             write(1, &tempchar,4);
-	     counter++;
-         } else if (*format == 's')
-         {
-             format++;
-             temp = va_arg(args,char*);
-             templen = strlen(temp);
-             write(1, temp,templen);
-	     counter++;
-         }
-         }
-        }
+		{
+			write(1,format,1);
+			counter ++;
+			format++;
+		}else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				format++;
+				tempchar = va_arg(args,int);
+				write(1, &tempchar,4);
+				counter++;
+			} else if (*format == 's')
+			{
+				format++;
+				temp = va_arg(args,char*);
+				templen = strlen(temp);
+				write(1, temp,templen);
+				counter++;
+			}else
+			{
+				write(1,format,1);
+				counter++;
+				format++;
+			}
+		}
+	}
 	return counter;
 }
