f7ced15ec5148eb25da1caf68b96785976999619
MohamedAlaga <malaga.142014@gmail.com>
2023-09-18 19:22:31 +0300

    done

--- printchar.c
+++ printchar.c
@@ -2,13 +2,14 @@
 /**
  * printchar - print charecter
  *@c : char to be printed
+ * Return: 1 if prints
  */
 int printchar(char c)
 {
 if (isgraph(c) || c == ' ')
 {
 write(1, &c, 1);
-return (1);}
+return (1); }
 else
 {
 write(1, "", 1);
