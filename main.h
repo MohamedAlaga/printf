317111ea919a538c3f9839d4b3c208bda667fb38
MohamedAlaga <malaga.142014@gmail.com>
2023-09-18 03:42:55 +0300

    .

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include<stdio.h>
-#include<string.h>
-#include<stdarg.h>
-#include<unistd.h>
-
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
 int _printf(const char *format, ...);
 
 
