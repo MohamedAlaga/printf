f4dd19b89d7546ba2a8d5b32014e09ec58c62503
MohamedAlaga <malaga.142014@gmail.com>
2023-09-18 04:08:14 +0300

    .

--- printf.c
+++ printf.c
@@ -1,45 +1,55 @@
 #include "main.h"
 void printchar(char c)
-{if (isgraph(c))
-write(1, &c, 1) ;
+{
+if (isgraph(c))
+write(1, &c, 1);
 else
-{write(1," ",1);}
+{write(1, " ", 1); }
 }
 void printstr(char *str)
-{ if (isgraph(*str))
+{
+if (isgraph(*str))
 while(*str)
 { 
 printchar(*str);
 str++;
 }
- else {write(1,"(null)", 6) ;};
-}
+else 
+{
+write(1, "(null)", 6) ; }}
 int stringlen(char *str)
 {
-  if (isgraph(*str))
-  {return (strlen(str));}
-  else return(6);
+if (isgraph(*str))
+{return (strlen(str)); }
+else return(6);
 }
 int _printf(const char *format, ...)
 {
 int counter = 0;
-char *temp ;
+char *temp;
 va_list args;
-va_start(args,format);
-while(*format != '\0'){
-if(*format == '%')
-{ 
-format ++;
-switch (*format){
-case 'c' : printchar(va_arg(args,int));;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c': printchar(va_arg(args, int));
 break;
-case '%' : printchar('%');;
+case '%': printchar('%');
 break;
-case 's' : temp = va_arg(args,char *); printstr(temp);counter += stringlen(temp)-1;
+case 's': temp = va_arg(args,char *);
+printstr(temp);
+counter += stringlen(temp)-1;
+break;
 }  
 }
-else{write(1,format,1);}
-format ++;
+else
+{
+write(1, format, 1);}
+format++;
 counter++;
 }
 return (counter);
