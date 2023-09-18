6f88ad806000e649ebf20b3eba7d1ec236d76d27
MohamedAlaga <malaga.142014@gmail.com>
2023-09-18 18:24:06 +0300

    .

--- stringlen.c
+++ stringlen.c
@@ -5,10 +5,9 @@
  *
  * Return: length of th string
  */
-int stringlen(char *str)
-{
-if (isgraph(*str))
-{return (strlen(str)); }
-else
-return (6);
+int stringlen(char *str) {
+  if (isgraph(*str)) {
+    return (strlen(str));
+  } else
+    return (6);
 }
