23b8878aaecee5a64614730350d3c58949677d4f
MohamedAlaga <malaga.142014@gmail.com>
2023-09-18 18:48:21 +0300

    done

--- printchar.c
+++ printchar.c
@@ -2,10 +2,11 @@
 /**
  * printchar - print charecter
  *@c : char to be printed
+ *@c2 : char to check first
  */
-void printchar(char c,char c2)
+void printchar(char c, char c2)
 {
-if (isgraph(c)&&(c == c2))
+if (isgraph(c) && (c == c2))
 write(1, &c, 1);
 else
 {write(1, " ", 1); }
--- printstr.c
+++ printstr.c
@@ -2,19 +2,21 @@
 /**
  * printstr - print all the string
  *@str : string to be printed
+ *@str2 : string to check the arg
  *
+ *Return: number of charecters printed
  */
-int printstr(char *str ,char *str2)
+int printstr(char *str, char *str2)
 {int counter = 0;
 if (isgraph(*str) && (str == str2))
 while (*str)
 {
-printchar(*str,*str);
+printchar(*str, *str);
 str++;
 counter++;
 }
 else
 {
-write(1, "(null)", 6) ; 
-counter = 6;}
-return(counter);}
+write(1, "(null)", 6);
+counter = 6; }
+return (counter); }
