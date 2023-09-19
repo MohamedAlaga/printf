96a6df45a33b192efb2b5683b4dad463bb939f03
mashaheen <finalflash147@yahoo.com>
2023-09-19 15:17:03 -0700

    a

--- printnum.c
+++ printnum.c
@@ -2,6 +2,9 @@
 int printnum(int n)
 {
 int len =0,temp;
+
+if (isdigit(n))
+{
 while(n)
 {
 if (n < 0){
@@ -11,6 +14,7 @@ n=n*(-1);
 temp = n%10;
 printchar(temp+'0');
 n=n/10;
+}
 }
 return len;
 }
