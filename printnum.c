5f8d56da1f8b46fc7e6dd718b03105d2c5b8d7af
mashaheen <finalflash147@yahoo.com>
2023-09-19 16:29:22 -0700

    a

--- printnum.c
+++ printnum.c
@@ -2,19 +2,24 @@
 int printnum(int n)
 {
 int len =0,temp;
+int forcount = n, rest = 0, multi = 1;
 
-if (isdigit(n))
-{
-while(n)
-{
 if (n < 0){
 printchar('-');
 n=n*(-1);
 }
-temp = n%10;
-printchar(temp+'0');
-n=n/10;
+while(forcount)
+{
+forcount = forcount /10;
+len++;
 }
+multi = pow(10,(len - 1));
+for (int i = 0; i < len; i++)
+{
+temp = n / multi;
+n = n % multi;
+multi = multi / 10;
+printchar(temp+'0');
 }
 return len;
 }
